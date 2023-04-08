1639f2480508db826dcb816aed40b829ede4969a
Jatin <jatin24ai024@satiengg.in>
2023-04-09 00:09:22 +0530

    gradio successfully setted up

--- client/app.py
+++ client/app.py
@@ -2,17 +2,46 @@ import gradio as gr
 import requests
 import numpy as np
 from PIL import Image
+from io import BytesIO
+import PIL 
+import os
+import json
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+def sentence_builder(age, sex, skin_type, allergies, diet, file):
+    print(age, sex, skin_type, allergies, diet)
+    
+    img_byte_arr = BytesIO()
+    file.save(img_byte_arr, format='JPEG')
+    img_byte_arr = img_byte_arr.getvalue()
+    payload = {'file': ('image.jpg', img_byte_arr, 'image/jpeg')}
 
+    response = requests.post('http://127.0.0.1:5000/predict', files=payload)
+    data = response.json()
+    
+    response = requests.post('http://127.0.0.1:5000/recommendation', json=data)
+    data = response.json()
 
-def predict(input_img_numpy_array):
-    img = Image.fromarray(input_img_numpy_array)
-    x = requests.post('http://127.0.0.1:5000/predict', files=img)
-    print(x)
-    return img
+    content = data['choices'][0]['message']['content']
+    
+    return content
 
+demo = gr.Interface(
+    sentence_builder,
+    [
+        gr.Number(value=20, label="Age"),
+        gr.Radio(["Male", "Female", "Other"], label="Gender", info="Your Gender"),
+        gr.CheckboxGroup(["Oily", "Dry", "Normal"], label="Skin", info="Skin Type"), 
+        gr.Dropdown(
+            ["benzoyl peroxide", "salicylic acid", "Sun-exposure", "Itching", "Swelling", "Redness"], 
+            multiselect=True, label="Allergies", 
+            info="Tell us your allergies and symptoms"
+        ),
+        gr.CheckboxGroup(["Veg", "Non-Veg",], label="Diet", info="Select your diet preference"),
+        gr.Image(type="pil", label="Face Image (with open eye)"),
+    ],
+    "text",
+)
 
-
-demo = gr.Interface(fn=predict, inputs=gr.Image(shape=(200, 200)) , outputs="image")
-
-demo.launch()   
+if __name__ == "__main__":
+    demo.launch()   
