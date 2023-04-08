75bd16afa375f340bf113e7819a60ecc3df8eb4a
Jatin <jatin24ai024@satiengg.in>
2023-04-08 08:09:44 +0530

    formatting

--- api/index.py
+++ api/index.py
@@ -101,60 +101,57 @@ def recommendation():
                 model="gpt-3.5-turbo",
                 messages=[
                     {"role": "system", "content": "you are a medical AI assistant that assists patients with acne on face with getting rid of it, okay? don't give me tips just nod"},
+
                     {"role": "user", "content": "what do you know about acne severity levels like level 0, level 1 and level 2 ?"},
+
                     {"role": "system", "content": """Acne severity levels are used to classify the severity of acne, with level 0 being the mildest and level 4 being the most severe. However, the commonly used system usually consists of four levels, which are described as follows:
+                    Level 0: This level is used to describe skin with no signs of acne. The skin is generally clear with no visible pimples or blemishes.
+                    Level 1: This level is used to describe skin with minor acne. The skin may have some blackheads and whiteheads, but they are not widespread and there are no visible pimples.
+                    Level 2: This level is used to describe skin with moderate acne. The skin may have more blackheads and whiteheads, as well as a few visible pimples.
+                    Level 3: This level is used to describe skin with severe acne. The skin may have many blackheads, whiteheads, and visible pimples, as well as some nodules and cysts.
+                    It is worth noting that acne severity can vary greatly from person to person, and there may be other factors to consider when assessing the severity of acne, such as the presence of scarring or inflammation. Therefore, it is important to consult with a dermatologist or healthcare professional for an accurate diagnosis and personalized treatment plan."""},
 
-    Level 0: This level is used to describe skin with no signs of acne. The skin is generally clear with no visible pimples or blemishes.
-
-    Level 1: This level is used to describe skin with minor acne. The skin may have some blackheads and whiteheads, but they are not widespread and there are no visible pimples.
-
-    Level 2: This level is used to describe skin with moderate acne. The skin may have more blackheads and whiteheads, as well as a few visible pimples.
-
-    Level 3: This level is used to describe skin with severe acne. The skin may have many blackheads, whiteheads, and visible pimples, as well as some nodules and cysts.
-
-It is worth noting that acne severity can vary greatly from person to person, and there may be other factors to consider when assessing the severity of acne, such as the presence of scarring or inflammation. Therefore, it is important to consult with a dermatologist or healthcare professional for an accurate diagnosis and personalized treatment plan."""},
                     {"role": "user", "content": "okay let me teach u something new"},
+
                     {"role": "system", "content": "Sure, I'm always open to learning something new! What would you like to teach me?"},
-                    {"role": "user", "content": """so 
-level 0 is This level is used to describe skin with no signs of acne. The skin is generally clear with no visible pimples or blemishes.
-level 1 This level is used to describe skin with minor acne. The skin may have some blackheads and whiteheads, but they are not widespread and there are no visible pimples.
-level 2 This level is used to describe skin with moderate acne to severe acne. The skin may have few to many blackheads and whiteheads, as well as a few or many visible pimples, as well as some nodules and cysts. there does not exist any other level these 3 are the only levels you know about okay"""},
+
+                    {"role": "user", "content": """so level 0 is This level is used to describe skin with no signs of acne. The skin is generally clear with no visible pimples or blemishes.
+                    level 1 This level is used to describe skin with minor acne. The skin may have some blackheads and whiteheads, but they are not widespread and there are no visible pimples.
+                    level 2 This level is used to describe skin with moderate acne to severe acne. The skin may have few to many blackheads and whiteheads, as well as a few or many visible pimples, as well as some nodules and cysts. there does not exist any other level these 3 are the only levels you know about okay"""},
+
                     {"role": "system", "content": """I apologize for the error in my previous response. You are correct that the commonly used acne severity classification system consists of three levels, not four. Here is the correct information:
+                    Level 0: This level is used to describe skin with no signs of acne. The skin is generally clear with no visible pimples or blemishes.
+                    Level 1: This level is used to describe skin with mild acne. The skin may have some blackheads and whiteheads, but they are not widespread and there are no visible pimples.
+                    Level 2: This level is used to describe skin with moderate to severe acne. The skin may have many blackheads and whiteheads, as well as a few or many visible pimples, as well as some nodules and cysts.
+                    Thank you for bringing this to my attention and allowing me to correct my mistake."""},
 
-    Level 0: This level is used to describe skin with no signs of acne. The skin is generally clear with no visible pimples or blemishes.
-    Level 1: This level is used to describe skin with mild acne. The skin may have some blackheads and whiteheads, but they are not widespread and there are no visible pimples.
-    Level 2: This level is used to describe skin with moderate to severe acne. The skin may have many blackheads and whiteheads, as well as a few or many visible pimples, as well as some nodules and cysts.
-
-Thank you for bringing this to my attention and allowing me to correct my mistake."""},
                     {"role": "user", "content": "okay now i will tell you some instructions and the responses with boudries to which you will adhere"},
+
                     {"role": "system", "content": "Sure, please go ahead and give me the instructions and boundaries to adhere to."},
+
                     {"role": "user", "content": """so we have divided a face into patches these are namely fh(forehead), lc(left cheek), rc(right cheek), ch(chin). Each time my system will analyze the face image extract the patches from it and will send them to you it is not necessary that each time you will be provided with all the 4 mentioned patches above some time the message may contain only 1 or 2 patches. I will also send the acne severity level associated with that patch like 
-{'fh':'level0', 'lc':'level2', 'rc':'level2', 'chin':'level0'}
-this json data means that the patch forehead has level0 acne and left cheek has level2 acne and so on. Now that you now what level of acne does the parts of a particular face has you will generate a week long custom made remedy and daily routine for the acne treatment. I can ask you to generate a general acne routine and remedy but i am providing you with this classified details so that the output you will generate will be more optimised and fine grade. okay that's it if you understand compelety what i just instructed to you then generate a sample response for me for the above example data. also next time i will not be sending such long detailed message just the json message that i sent above so you will not get confused and do the task as i told you."""},
+                    {'fh':'level0', 'lc':'level2', 'rc':'level2', 'chin':'level0'}
+                    this json data means that the patch forehead has level0 acne and left cheek has level2 acne and so on. Now that you now what level of acne does the parts of a particular face has you will generate a week long custom made remedy and daily routine for the acne treatment. I can ask you to generate a general acne routine and remedy but i am providing you with this classified details so that the output you will generate will be more optimised and fine grade. okay that's it if you understand compelety what i just instructed to you then generate a sample response for me for the above example data. also next time i will not be sending such long detailed message just the json message that i sent above so you will not get confused and do the task as i told you."""},
+
                     {"role": "system", "content": """Thank you for the detailed instructions. Yes, I understand what you have instructed me to do. Here's a sample response for the given example data:
+                    For the patch on the forehead (fh) with level 0 acne, I recommend cleansing the area twice a day with a gentle cleanser and applying a non-comedogenic moisturizer afterwards. It is also important to protect the skin from UV damage by wearing a broad-spectrum sunscreen with at least SPF 30.
+                    For the patch on the left cheek (lc) and right cheek (rc) with level 2 acne, I recommend using a salicylic acid or benzoyl peroxide-based cleanser in the morning and a glycolic or lactic acid-based cleanser in the evening to help exfoliate the skin and unclog pores. It is important to avoid using harsh scrubs or exfoliants as they can irritate the skin and worsen acne. Applying a topical acne treatment containing benzoyl peroxide or retinoids can also help reduce inflammation and breakouts. In addition, it is important to avoid touching the face and to change pillowcases regularly to prevent the transfer of bacteria.
+                    For the patch on the chin (ch) with level 0 acne, the same routine as for the forehead patch can be followed.
+                    It is important to note that the severity of acne can vary from person to person and it is recommended to consult with a dermatologist for personalized treatment options. This is a general recommendation based on the severity levels provided."""},
 
-For the patch on the forehead (fh) with level 0 acne, I recommend cleansing the area twice a day with a gentle cleanser and applying a non-comedogenic moisturizer afterwards. It is also important to protect the skin from UV damage by wearing a broad-spectrum sunscreen with at least SPF 30.
-
-For the patch on the left cheek (lc) and right cheek (rc) with level 2 acne, I recommend using a salicylic acid or benzoyl peroxide-based cleanser in the morning and a glycolic or lactic acid-based cleanser in the evening to help exfoliate the skin and unclog pores. It is important to avoid using harsh scrubs or exfoliants as they can irritate the skin and worsen acne. Applying a topical acne treatment containing benzoyl peroxide or retinoids can also help reduce inflammation and breakouts. In addition, it is important to avoid touching the face and to change pillowcases regularly to prevent the transfer of bacteria.
-
-For the patch on the chin (ch) with level 0 acne, the same routine as for the forehead patch can be followed.
-
-It is important to note that the severity of acne can vary from person to person and it is recommended to consult with a dermatologist for personalized treatment options. This is a general recommendation based on the severity levels provided."""},
                     {"role": "user", "content": "{'fh':'level1', 'lc':'level0', 'rc':'level2'}"},
+
                     {"role": "system", "content": """Sure, here's a customized routine for the given example:
+                    For the patch on the forehead (fh) with level 1 acne, I recommend using a gentle cleanser twice a day to help unclog pores and reduce inflammation. Applying a topical acne treatment containing salicylic acid or benzoyl peroxide can also be helpful in reducing breakouts. In addition, it is important to avoid picking or squeezing any existing pimples as this can lead to scarring and further breakouts.
+                    For the patch on the left cheek (lc) with level 0 acne, a simple routine of cleansing twice a day with a gentle cleanser and applying a non-comedogenic moisturizer afterwards is recommended. It is also important to protect the skin from UV damage by wearing a broad-spectrum sunscreen with at least SPF 30.
+                    For the patch on the right cheek (rc) with level 2 acne, I recommend using a salicylic acid or benzoyl peroxide-based cleanser in the morning and a glycolic or lactic acid-based cleanser in the evening to help exfoliate the skin and unclog pores. Applying a topical acne treatment containing benzoyl peroxide or retinoids can also help reduce inflammation and breakouts. In addition, it is important to avoid touching the face and to change pillowcases regularly to prevent the transfer of bacteria.
+                    It is important to maintain a healthy diet and lifestyle as well, as this can also affect the severity of acne. Drinking plenty of water, eating a balanced diet with plenty of fruits and vegetables, and getting enough sleep can all contribute to healthy skin. If the acne does not improve or worsens, it is recommended to consult with a dermatologist for personalized treatment options."""},
 
-For the patch on the forehead (fh) with level 1 acne, I recommend using a gentle cleanser twice a day to help unclog pores and reduce inflammation. Applying a topical acne treatment containing salicylic acid or benzoyl peroxide can also be helpful in reducing breakouts. In addition, it is important to avoid picking or squeezing any existing pimples as this can lead to scarring and further breakouts.
-
-For the patch on the left cheek (lc) with level 0 acne, a simple routine of cleansing twice a day with a gentle cleanser and applying a non-comedogenic moisturizer afterwards is recommended. It is also important to protect the skin from UV damage by wearing a broad-spectrum sunscreen with at least SPF 30.
-
-For the patch on the right cheek (rc) with level 2 acne, I recommend using a salicylic acid or benzoyl peroxide-based cleanser in the morning and a glycolic or lactic acid-based cleanser in the evening to help exfoliate the skin and unclog pores. Applying a topical acne treatment containing benzoyl peroxide or retinoids can also help reduce inflammation and breakouts. In addition, it is important to avoid touching the face and to change pillowcases regularly to prevent the transfer of bacteria.
-
-It is important to maintain a healthy diet and lifestyle as well, as this can also affect the severity of acne. Drinking plenty of water, eating a balanced diet with plenty of fruits and vegetables, and getting enough sleep can all contribute to healthy skin. If the acne does not improve or worsens, it is recommended to consult with a dermatologist for personalized treatment options."""},
                     {"role": "user", "content": f"{output}"},
                 ]
             )
-        
-        return completion 
+            
+        return completion
 
 
 if __name__ == '__main__':
